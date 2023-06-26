c27644b8c7f6a5dbf7e6a396461d8206bfade254
abay <35039013+abaykan@users.noreply.github.com>
2023-06-26 17:17:50 +0700

    Create extract-endpoints-from-js.sh: Extract Endpoints From JS Files List

---
+++ extract-endpoints-from-js.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Get extract.rb from https://github.com/jobertabma/relative-url-extractor/blob/master/extract.rb
+#
+# Usage: ./extract-endpoints-from-js.sh list.txt
+
+GREEN='\033[0;32m'  # ANSI escape sequence for green color
+NC='\033[0m'       # ANSI escape sequence to reset color
+
+echo -e " /\_/\  "
+echo -e "( o.o )  Extract Endpoint(s) from JS Files~"
+echo -e " > ^ < "
+echo "Optional: -o output.txt -silent only show output"
+echo -e "---------------------------------------------"
+echo -e "\nList: $1\n"
+
+# Check if the input file argument is provided
+if [ -z "$1" ]; then
+  echo "Please provide the path to the input file."
+  exit 1
+fi
+
+# Assign the input file argument to a variable
+input_file="$1"
+
+# Replace 'extract.rb' with the path to your 'extract.rb' script
+extract_script="extract.rb"
+
+# Check if the -o option is provided with a filename
+if [ "$2" == "-o" ] && [ ! -z "$3" ]; then
+  output_file="$3"
+else
+  random_string=$(date +%s%N | md5sum | awk '{print $1}')
+  output_file="/tmp/$random_string.txt"
+fi
+
+# Check if the -silent option is provided
+if [ "$4" == "-silent" ]; then
+  silent_mode=true
+else
+  silent_mode=false
+fi
+
+# Read the input file line by line
+while IFS= read -r line; do
+  if [ "$silent_mode" = false ]; then
+  	echo -e "${GREEN}URL${NC}: $line"
+  fi
+  # Perform curl command with the line as a parameter and pipe the output to extract.rb
+  output=$(curl -s "$line" | "$extract_script")
+
+  # Check if the output contains a forward slash (/)
+  if [[ $output == *"/"* ]]; then
+    output_length=${#output}
+    if (( output_length <= 500 )); then
+      echo "$output" | tee -a "$output_file"
+    fi
+  fi
+done < "$input_file"
+
+echo -e "\nOutput File: $output_file"
